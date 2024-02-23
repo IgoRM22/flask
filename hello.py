f888da3a5ed8fac011fc00d69c79e808a9c48654
IgoRM22 <i.ramos@aluno.ifsp.edu.br>
2024-02-22 21:21:08 -0300

    Teste 2

--- hello.py
+++ hello.py
@@ -5,18 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Lucas_Sousa/PT3019578/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
+    return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Igor_Ramos/PT3019284/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
 
-@app.route("/user/Lucas_Sousa/PT3019578/IFSP")
+@app.route("/user/Igor_Ramos/PT3019284/IFSP")
 def user():
-return '<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: Lucas_Sousa</h2><h2>Prontuário: PT3019578</h2><h2>Instituição: IFSP</h2><p><a href="/">Voltar</a></p>'
+    return '<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: Igor_Ramos</h2><h2>Prontuário: PT3019284</h2><h2>Instituição: IFSP</h2><p><a href="/">Voltar</a></p>'
 
 @app.route("/contextorequisicao")
 def context():
-user_agent = request.headers.get('User-Agent')
-url = request.host_url
-ip = request.host_addr
-return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {}</h2><h2>O IP do computador remoto é: {}</h2><h2>O host da aplicação é: {}</h2><p><a href="/">Voltar</a></p>'.format(user_agent, url, ip)
+    user_agent = request.headers.get('User-Agent')
+    url = request.host_url
+    ip = request.host_url
+    return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {}</h2><h2>O IP do computador remoto é: {}</h2><h2>O host da aplicação é: {}</h2><p><a href="/">Voltar</a></p>'.format(user_agent, url, ip)
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
