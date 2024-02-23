41818e59f69c37ab9ca64ccdec3b1a17746881e7
IgoRM22 <i.ramos@aluno.ifsp.edu.br>
2024-02-22 21:12:58 -0300

    Modificação professor

--- hello.py
+++ hello.py
@@ -1,6 +1,22 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():
-    return '<p>Teste!</p><table><tr><td><b>Aluno:</b></td><td>Igor Ramos Almeida</td></tr><tr><td><b>Prontuário:</b></td><td>PT3019284</td></tr></table>'
+def index():
+return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Lucas_Sousa/PT3019578/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
+
+@app.route("/user/Lucas_Sousa/PT3019578/IFSP")
+def user():
+return '<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: Lucas_Sousa</h2><h2>Prontuário: PT3019578</h2><h2>Instituição: IFSP</h2><p><a href="/">Voltar</a></p>'
+
+@app.route("/contextorequisicao")
+def context():
+user_agent = request.headers.get('User-Agent')
+url = request.host_url
+ip = request.host_addr
+return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {}</h2><h2>O IP do computador remoto é: {}</h2><h2>O host da aplicação é: {}</h2><p><a href="/">Voltar</a></p>'.format(user_agent, url, ip)
+
+if __name__ == '__main__':
+app.run(host='0.0.0.0', port=80)
